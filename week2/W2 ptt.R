a83fbd31d2885970a3ce56d75408a18ef0b7b512
Karineyeng <40814158+Karineyeng@users.noreply.github.com>
2018-07-11 19:47:25 +0800

    week2 text mining

---
+++ week2/W2 ptt.R
@@ -0,0 +1,31 @@
+library(xml2)
+library(tmcn)
+
+pttTestFunction <- function(URL, filename)
+{
+  #URL   = "https://www.ptt.cc/bbs/NTUcourse/index.html"
+  html  = read_html(URL)
+  title = html_nodes(html, "a")
+  href  = html_attr(title, "href")
+  data = data.frame(title = toUTF8(html_text(title)),
+                       href = href)
+  data = data[-c(1:10),]
+  getContent <- function(x) {
+    url  = paste0("https://www.ptt.cc", x)
+    tag  = html_node(read_html(url), 'div#main-content.bbs-screen.bbs-content')
+    text = toUTF8(html_text(tag))
+  }
+  #getContent(data$href[1])
+  allText = sapply(data$href, getContent)
+  allText
+  #out <- file(filename, "w", encoding="BIG-5") 
+  write.table(allText, filename) 
+  #close(out) 
+}
+
+id = c(1:10)
+URL = paste0("https://www.ptt.cc/bbs/NTUcourse/index", id, ".html")
+filename = paste0(id, ".txt")
+pttTestFunction(URL[1], filename[1])
+mapply(pttTestFunction, 
+       URL = URL, filename = filename)
